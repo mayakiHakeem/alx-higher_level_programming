226cbd6ea615716588794d1c3a6a0a5f2e38b3b5
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-13 14:34:19 +0100

    Add script to rev and alternate alphabet printing

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for chara in range(122, 96, -1):
-   if chara % 2 != 0:
+    if chara % 2 != 0:
         chara -= 32
-   print("{}".format(chr(chara)), end='')
-        
+    print("{}".format(chr(chara)), end='')
