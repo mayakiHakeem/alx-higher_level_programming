b657f74b7f0912cd514a3708cfdaf6f4cdbde36a
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-13 15:03:33 +0100

    Add python script to solve task 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,4 +3,4 @@ def remove_char_at(str, n):
     if n < 0 or n > (len(str) - 1):
         return str
     else:
-        return ("{}{}".format(str[0:n],str[n+1:]))
+        return ("{}{}".format(str[0:n], str[n+1:]))
