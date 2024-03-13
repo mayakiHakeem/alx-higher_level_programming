90479d34e899e9302dc8ffd7bdf6f831dde0cb14
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-13 11:05:51 +0100

    Add files to print combs of numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(1, 10):
-        if i < j and (i != 8 and j != 9):
-            print("{}{},".format(i, j), end=' ')
-print("{}{}".format(i - 1, j))
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i != j:
+            if not(i == 8 and j == 9):
+                print("{:02d}, ".format(i * 10 + j), end='')
+            else:
+                print("{:02d}".format(i * 10 + j))
