92d3febe01bfb6fdb3ee9d2fdbe0c1b613bf85e9
mayakiHakeem <mayakijnr@gmail.com>
2024-03-14 23:38:24 +0100

    add script to handle commandline args in programs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     if length < 2:
         print("{} arguments.".format(length - 1))
     elif length == 2:
-        print("{} argument.".format(length - 1))
+        print("{} argument:".format(length - 1))
         print("{}: {}".format(1, sys.argv[1]))
     else:
-        print("{} arguments.".format(length - 1))
+        print("{} arguments:".format(length - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
