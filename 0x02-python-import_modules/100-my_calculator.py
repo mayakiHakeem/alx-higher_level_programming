26df1972fc280fcd79f7f0f43581b0512a8f3629
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-15 14:37:13 +0100

    Add scrypt to run basic calculation in the command line

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,22 +6,17 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     length = len(sys.argv)
-    result = 0
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
 
     if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    else:
-        if sys.argv[2] == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif sys.argv[2] == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif sys.argv[2] == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif sys.argv[2] == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            
-        
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    
+    operators = {"+":add, "-":sub, "*":mul, "/":div}
+    
+    if sys.argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+   
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
