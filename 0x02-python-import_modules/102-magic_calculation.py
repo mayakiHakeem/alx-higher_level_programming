687afdb2f04b601c4ba5de2a45d59fd0dab18125
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-17 15:05:18 +0100

    Add solution to writing function given bytecode

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    from magic_calculation import add, sub
+
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+            return c
+        else:
+            return sub(a, b)
+                                        
