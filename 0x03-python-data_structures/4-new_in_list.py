5701d2316afd2000cfb62e37ba097e9bfab6291c
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-18 18:30:55 +0100

    Fixed task 3 format

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def new_in_list(my_list, idx, element):
     if my_list:
         if idx < 0 or idx > len(my_list)-1:
             return my_list
-        a_list = my_list{::}
+        a_list = my_list[::]
         a_list[idx] = element
         return a_list
