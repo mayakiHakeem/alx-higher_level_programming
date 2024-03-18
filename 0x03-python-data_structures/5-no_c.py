f8a5f1a2172d25e02fcbd64fbb778c6da01da607
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-18 18:58:13 +0100

    Resolved req errs for task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@
 def no_c(my_string):
     ctring = ""
     for char in my_string:
-        if char != 'c' or char != 'C':
+        if char != 'c' and char != 'C':
             ctring += char
     return ctring
