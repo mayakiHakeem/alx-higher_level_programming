bbb69bba399ba6d9b0aa17bb77aae362bbe8e4a0
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-18 18:59:18 +0100

    Resolved req errs for task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for sub_mat in matrix:
         for num in sub_mat:
             if num != sub_mat[-1]:
-                print("{} ".format(num), end='')
+                print("{:d} ".format(num), end='')
             else:
-                print("{}".format(num), end='')
+                print("{:d}".format(num), end='')
         print()
