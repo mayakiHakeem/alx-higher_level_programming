6761c9c5952a488c3d7c6609ba223582de4096d2
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-21 12:24:31 +0100

    Fixed used set for task 2 instead of list comprehension

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,9 +2,8 @@
 
 
 def uniq_add(my_list=[]):
-    if my_list:
-        new_list = set(my_list)
-        result = 0
-        for x in new_list:
-            result += x
+    new_list = set(my_list)
+    result = 0
+    for x in new_list:
+        result += x
     return result
