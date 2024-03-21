67437b33ef0fb42f828e06c953cdf856fb8fe02f
Mayaki Hakeem <mayakijnr@gmail.com>
2024-03-21 11:57:43 +0100

    Add script to solve for the task 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    if my_list:
+        for item in my_list:
+            if item == search:
+                item = replace
+            new_list.append(item)
+    return new_list
