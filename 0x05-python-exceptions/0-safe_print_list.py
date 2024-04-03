5f1123510bc6a8e7044bf64f10efbc7c0ed41f9a
Mayaki Hakeem <mayakijnr@gmail.com>
2024-04-03 17:47:11 +0100

    Fix newline printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,7 +16,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             count += 1
-        print()
     except Exception:
         pass
+    print()
     return count
