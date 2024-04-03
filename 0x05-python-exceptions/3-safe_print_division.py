7145f3ca35df845c6296baab3b79c02e8dc34c03
Mayaki Hakeem <mayakijnr@gmail.com>
2024-04-03 18:53:45 +0100

    Add files to solve task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -13,8 +13,8 @@ def safe_print_division(a, b):
         result.
     """
     try:
-        result = a / b;
-    except:
+        result = a / b
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
