73409af8bde582223a5e56abfd451ef03a2823a1
Mayaki Hakeem <mayakijnr@gmail.com>
2024-04-03 17:37:35 +0100

    Add files for exception task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+        print()
+    except Exception:
+        pass
+    return count
