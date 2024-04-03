fdb3c2356f336efe1ab65091a3a29a2bf3ccd074
Mayaki Hakeem <mayakijnr@gmail.com>
2024-04-03 18:50:58 +0100

    Add file to solve task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    """
+    Divide 2 integers and print result.
+
+    Args:
+        a (int): a
+        b (int): b
+
+    Returns:
+        result.
+    """
+    try:
+        result = a / b;
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
