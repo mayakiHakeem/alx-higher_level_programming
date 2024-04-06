07ab09797b2c9f96a78606d8bb6650e542edd4ff
mayakiHakeem <mayakijnr@gmail.com>
2024-04-06 23:28:54 +0100

    Add files to handle advancebtask 100 of Exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -14,7 +14,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception:
-        print("Exception: Unknown format code 'd' for object of type 'str'")
+        print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
         return False
     else:
         return True
