c563cc23ea10eff75a35f28133730826160554ec
mayakiHakeem <mayakijnr@gmail.com>
2024-04-06 23:46:33 +0100

    Add files to handle advancebtask 100 of Exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -13,7 +13,7 @@ def safe_print_integer_err(value):
     """
     try:
         print("{:d}".format(value))
-    except ValueError as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
     else:
