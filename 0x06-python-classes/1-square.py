5900f89c6871f02936bdf5a3e7bef41eb45d0b29
Mayaki Hakeem <mayakijnr@gmail.com>
2024-04-22 14:24:57 +0100

    Add solution to task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+"""Defined class Square"""
+
+
 class Square:
+    """Initializes private instance attribute
+    Attributes:
+       size (int): The size of the square
+    """
     def __init__(self, size):
         self.__size = size
