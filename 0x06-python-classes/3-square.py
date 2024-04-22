638cede6de248a53d9361ee6be1ca8fc72ce2a36
Mayaki Hakeem <mayakijnr@gmail.com>
2024-04-22 14:42:04 +0100

    Add solution to task 2

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+"""class that defines a square"""
+
+
 class Square:
+    """Initialize size of square and calc. the area
+    Attributes:
+        size (int): The size of the square
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
