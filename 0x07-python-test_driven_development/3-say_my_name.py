6bf7d016c5bdcfb6195e80b2abe0c9b42c628fb1
mayaki <mayakijnr@gmail.com>
2024-07-08 11:17:09 +0100

    Rewrit exercise 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""
-    A function that prints first_name and last_name
-    Exception:
-        raised TypeError if first or last_name is not string
-    >>> say_my_name("John", "Smith")
-    My name is John Smith
-    >>> say_my_name("Walter", "White")
-    My name is Walter White
-    >>> say_my_name("Bob")
-    My name is Bob
-    >>> try:
-    ...     say_my_name(12, "White")
-    ... except Exception as e:
-    ...     print(e)
-    first_name must be a string
-"""
+# 3-say_my_name.py
+"""A function that join and print fullname"""
 
 
 def say_my_name(first_name, last_name=""):
-    """ A function that concatenate two string together
-        Args:
-            first_name: user first name
-            last_name: user last name
+    """
+    join and print full_name
+
+    Args:
+    first_name (str): first name to be written
+    last_name (str): last_name to be written
+
+    Returns:
+    None
+
+    Raises:
+    TypeError: first_name must be a string
+    TypeError: last_name must be a string
     """
     if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
-    elif not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
-    else:
-        print('My name is {} {}'.format(first_name, last_name))
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name + " " + last_name)
