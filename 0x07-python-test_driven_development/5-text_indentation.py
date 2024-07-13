6fb41a09e05f49b3ff22e44202b84605e21058bc
mayaki <mayakijnr@gmail.com>
2024-07-13 09:32:25 +0100

    Resolves doctest fail for task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 """
-    A function that prints a text with 2 new lines after each of these
-    characters: fullstop, question mark and colon.
-
-    Exceptions:
-        TypeError: if file is not a string type
+   This module contains a function that indent text.
 """
 
 
 def text_indentation(text):
-    """ A function that indent a text """
+    """A function that indent a text
+
+       Args:
+       text (str): text to indent
+
+       Exceptions:
+       TypeError: if text is not a string
+
+       Returns:
+       None
+    """
     delim = [':', '.', '?']
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    for i in range(len(text)):
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    for i in range(c, len(text)):
         if text[i] in delim:
             print(text[i] + "\n")
         elif text[i - 1] in delim and text[i] == " ":
