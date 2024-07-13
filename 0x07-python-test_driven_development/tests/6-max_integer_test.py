c2a31a4f3673d3b9cbf541e3fc31d0a157f959b7
mayaki <mayakijnr@gmail.com>
2024-07-13 10:32:59 +0100

    Add further cases to unittest 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,14 +11,16 @@ class TestMaxInteger(unittest.TestCase):
             If nothing is supplied return None
     """
     __test_cases = [
-            [1, 2, 3, 4],
-            [1, 3, 4, 2],
-            [],
-            [4.5, 2.4, 5.1, 3.0],
-            ['a', 'b', 'c', 'd']
+        [1, 2, 3, 4],
+        [1, 3, 4, 2],
+        [],
+        [4],
+        [4, 3, 1, 2],
+        [4.5, 2.4, 5.1, 3.0],
+        ['a', 'b', 'c', 'd']
         ]
     def test_max_integer(self):
-        cases = [4, 4, None, 5.1, 'd']
+        cases = [4, 4, None, 4, 4, 5.1, 'd']
         for i in range(len(self.__test_cases)):
             self.assertEqual(max_integer(self.__test_cases[i]), cases[i])
 
