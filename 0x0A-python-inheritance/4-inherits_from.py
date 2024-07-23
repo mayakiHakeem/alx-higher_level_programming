9c7ca7c36db0af8b54ec574b726b562ff5966392
mayaki <mayakijnr@gmail.com>
2024-07-23 18:14:32 +0100

    Solve task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin
+# 4-inherits_from.py
+"""A module that check class of an object"""
+
+
+def inherits_from(obj, a_class):
+    """checks if an obj is of a class that inherits directly from a_class
+
+    Args:
+        obj: object to check class' inheritance
+        a_class: class to check with
+
+    Returns:
+        True if object's class inherits directly from a_class; False otherwise
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
