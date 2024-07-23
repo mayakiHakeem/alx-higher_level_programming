61a89cac62b8ce741f42166057e47434561ce604
mayaki <mayakijnr@gmail.com>
2024-07-23 16:23:29 +0100

    Fix pycodestyle errs

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,13 +4,13 @@
 
 
 def is_same_class(obj, a_class):
-  """Check if obj is an exact instance of class a_class
+    """Check if obj is an exact instance of class a_class
 
-  Args:
-    obj: The object to check.
-    a_class: The class to compare against.
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
 
-  Returns:
-    True if obj is an exact instance of a_class, False otherwise.
-  """
-  return type(obj) ==  a_class
+    Returns:
+        True if obj is an exact instance of a_class, False otherwise.
+    """
+    return type(obj) == a_class
