b63c76b45a6a022a08fd70310ce6e431717a7d83
mayaki <mayakijnr@gmail.com>
2024-07-23 18:31:02 +0100

    Solve task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+"""Define a geometry module"""
+
+
+class BaseGeometry():
+    """A geometry based class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
