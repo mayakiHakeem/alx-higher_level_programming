0465b765a9b7dad2c77b0e33c15aed12ba19762d
mayaki <mayakijnr@gmail.com>
2024-07-25 00:21:25 +0100

    Solves task 11 of project

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 10-square.py
+"""Module that define a square class off a rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class that defines a square"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
