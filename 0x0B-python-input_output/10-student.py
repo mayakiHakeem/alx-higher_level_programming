0ce41e8e8e20055c5183244f241161024e63515e
mayaki <mayakijnr@gmail.com>
2024-08-02 16:19:39 +0100

    Add task 9 to 11

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 10-student.py
+"""Module define a Student class"""
+
+
+class Student:
+    """Defines a Student object"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs:
+            attrs_dict = {}
+            for item in attrs:
+                if item in self.__dict__.keys():
+                    attrs_dict[item] = self.__dict__[item]
+            return attrs_dict
+        else:
+            return self.__dict__
---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# 11-student.py
+"""Module define a Student class"""
+
+
+class Student:
+    """Defines a Student object"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs:
+            attrs_dict = {}
+            for item in attrs:
+                if item in self.__dict__.keys():
+                    attrs_dict[item] = self.__dict__[item]
+            return attrs_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        at_dict = dict(json)
+        for key in at_dict.keys():
+            self.key = at_dict[key]
