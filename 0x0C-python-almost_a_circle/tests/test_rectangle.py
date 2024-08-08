9657900808fff7adff294edf472c67aac7ad21fb
mayaki <mayakijnr@gmail.com>
2024-08-08 23:16:33 +0100

    Task 2

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -34,7 +34,7 @@ class TestRectangle_Instantiation(unittest.TestCase):
         self.assertEqual(rect.height, 20)
         self.assertEqual(rect.x, 5)
         self.assertEqual(rect.y, 0)
-        self.assertEqual(rect.id, 1)
+        self.assertEqual(rect.id, 2)
 
     def test_4_args(self):
         rect = Rectangle(10, 20, 5, 7)
@@ -43,7 +43,7 @@ class TestRectangle_Instantiation(unittest.TestCase):
         self.assertEqual(rect.height, 20)
         self.assertEqual(rect.x, 5)
         self.assertEqual(rect.y, 7)
-        self.assertEqual(rect.id, 1)
+        self.assertEqual(rect.id, 3)
 
     def test_5_args(self):
         rect = Rectangle(10, 20, 5, 7, 49)
@@ -55,29 +55,77 @@ class TestRectangle_Instantiation(unittest.TestCase):
         self.assertEqual(rect.id, 49)
 
     def test_excess_args(self):
-        with self.assertEqual(TypeError):
+        with self.assertRaises(TypeError):
             rect = Rectangle(*(range(10)))
 
     def test_private_width(self):
         rect = Rectangle(10, 20, 5, 7, 49)
 
-        with self.assertEqual(TypeError):
+        with self.assertRaises(AttributeError):
             print(rect.__width)
 
     def test_private_height(self):
         rect = Rectangle(10, 20, 5, 7, 49)
 
-        with self.assertEqual(TypeError):
+        with self.assertRaises(AttributeError):
             print(rect.__height)
 
     def test_private_x(self):
         rect = Rectangle(10, 20, 5, 7, 49)
 
-        with self.assertEqual(TypeError):
+        with self.assertRaises(AttributeError):
             print(rect.__x)
 
     def test_private_y(self):
         rect = Rectangle(10, 20, 5, 7, 49)
 
-        with self.assertEqual(TypeError):
+        with self.assertRaises(AttributeError):
             print(rect.__y)
+
+class TestRectangle_width_validation(unittest.TestCase):
+
+    def test_neg_width(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(-10, 20)
+
+    def test_zero_width(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(0, 20)
+
+    def test_width_type(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle("Hello", 20)
+
+class TestRectangle_height_validation(unittest.TestCase):
+
+    def test_neg_height(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(10, -20)
+
+    def test_zero_height(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(10, 0)
+
+    def test_height_type(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(45, "PiedPiper")
+
+class TestRectangle_x_validation(unittest.TestCase):
+
+    def test_neg_x(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(10, 20, -5)
+
+    def test_x_type(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(10, 20, (34, 43))
+
+class TestRectangle_y_validation(unittest.TestCase):
+
+    def test_neg_y(self):
+        with self.assertRaises(ValueError):
+            rect = Rectangle(10, 20, 5, -25)
+
+    def test_y_type(self):
+        with self.assertRaises(TypeError):
+            rect = Rectangle(10, 20, 34, ["One", "Two", "Three"])
