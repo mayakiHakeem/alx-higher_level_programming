4a721a9897e47199ceeae6d880a9d5f4f6c7000d
mayaki <mayakijnr@gmail.com>
2024-08-09 17:56:19 +0100

    Create and test Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,26 @@ class Square(Rectangle):
     """Defines a Square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square instance with an optional ID.
+
+        Args:
+            size (int): The size of the square.
+            x (int, optional): The x-coordinate. Defaults to 0.
+            y (int, optional): The y-coordinate. Defaults to 0.
+            id (int, optional): The ID of the rectangle. Defaults to None.
+        """
         super().__init__(size, size, x=0, y=0, id=None)
         self.size = size
         self.x = x
         self.y = y
+
+    def __str__(self):
+        """Get string of Square obj.
+
+        Returns:
+            str_form (str): The string form of the Square ibj.
+        """
+        return (
+            "[Square] ({}) {}/{} - {}"
+            .format(self.id, self.x, self.y, self.size)
+            )
