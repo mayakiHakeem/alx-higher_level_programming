291475eae77c69a0b77cc98646c8c1cae891c97e
mayaki <mayakijnr@gmail.com>
2024-08-10 19:47:08 +0100

    Write json to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,3 +42,28 @@ class Base:
         if not all(isinstance(item, dict) for item in list_dictionaries):
             raise TypeError("list_dictionaries must only contain dictionaries")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Returns JSON string representation
+
+        Args:
+            list_objs (list): list of objs to convert to JSON
+
+        Raises:
+            TypeError: list_objs not a list or contain non-obj type element
+        """
+        if list_objs is None:
+            return "[]"
+
+        if not isinstance(list_objs, list):
+            raise TypeError("list_objs must be a list")
+        if not all([isinstance(item, Base) for item in list_objs]):
+            raise TypeError("every item of list_objs must be instances of Base")
+
+        with open(f"{cls.__name__}.json", 'w', encoding="UTF8") as n_file:
+            new_list = []
+            for item in list_objs:
+                it = item.to_dictionary()
+                new_list.append(it)
+            n_file.write(Base.to_json_string(new_list))
