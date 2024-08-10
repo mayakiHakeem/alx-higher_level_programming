cdf9eda608927bf90af36144f584a5ef960c205a
mayaki <mayakijnr@gmail.com>
2024-08-10 19:06:26 +0100

    Fix edge checkers case

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,6 +39,7 @@ class Base:
 
         if not isinstance(list_dictionaries, list):
             raise TypeError("list_dictionaries must be a list of dictionaries")
-        if not all(isinstance(item, dict) for item in list_dictionaries):
+        if len(list_dictionaries) > 1 and not all(
+                isinstance(item, dict) for item in list_dictionaries):
             raise TypeError("list_dictionaries must only contain dictionaries")
         return json.dumps(list_dictionaries)
